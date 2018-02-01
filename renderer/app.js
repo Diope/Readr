e5497f743926b56c519872d073774d90d95bb9db
Dion Pettaway <dionp@fullsail.edu>
2018-02-01 12:23:44 -0500

    Submission form is grabbing value

--- renderer/app.js
+++ renderer/app.js
@@ -1,8 +1,16 @@
 // Show the add modal
 $('.open-add-modal').click(() => {
-  $('#add-modal').addClass('is-active')
+  $('#add-modal').addClass('is-active');
 });
 // Hide the close modal
 $('.close-add-modal').click(() => {
-  $('#add-modal').removeClass('is-active')
+  $('#add-modal').removeClass('is-active');
 });
+
+// Add modal submission
+$('#add-button').click(() => {
+  let newItemURL = $('#item-input').val();
+  if (newItemURL) {
+    console.log(newItemURL);
+  }
+})
